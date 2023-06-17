64e53ecb843383401a45b539de0090e0926fbb1b
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-18 05:32:35 +0700

    Completed a quick setup for the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from "react";
+import { Layout } from "../components";
+import "tailwindcss/tailwind.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
 
 const posts = [
-  { title: "React Testing", excerpt: "React Testing is cool" },
-  { title: "React with Tailwind", excerpt: "React with Tailwind is cool" },
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
 ];
 
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
-      Hello
       <Head>
         <title>AboutTHAT</title>
       </Head>
+
       <h1>AboutTHAT</h1>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
