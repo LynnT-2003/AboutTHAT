2663114ec17a0d53fbb3b2095298ed9c3923543c
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-18 15:27:35 +0700

    GraphQL Data Fetching and some minor refactors

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+// ];
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -29,4 +30,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
