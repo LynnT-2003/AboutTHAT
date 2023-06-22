3173c0b9f14ebf616b6c0c9803fee1a43cea101e
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-23 05:12:25 +0700

    Rollback

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -44,14 +44,14 @@ const PostDetails = ({ post }) => {
 
 export default PostDetails;
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
     props: { post: data },
   };
 }
 
-export async function getServerSideProps() {
+export async function getStaticPaths() {
   const posts = await getPosts();
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
