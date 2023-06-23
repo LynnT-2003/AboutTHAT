3b1e8c4c56410fc2cd9116ba5fd107018717943c
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-23 15:18:48 +0700

    Console logging PostCard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import Link from "next/link";
 
 const PostCard = ({ post }) => {
-  console.log(post);
+  console.log("FETCHED POSTS IN POSTCARD:", post);
   return (
     <div className="bg-black bg-opacity-70 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
