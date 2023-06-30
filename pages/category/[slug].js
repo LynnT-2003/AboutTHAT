e592efbca2328734f8e162a936fd44b9e546b8fe
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-07-01 00:07:21 +0700

    Change to UseEffect

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { getCategories, getCategoryPost } from "../../services";
 import { PostCard, Categories, Loader } from "../../components";
 
-const CategoryPost = ({ posts }) => {
+const CategoryPost = () => {
   const router = useRouter();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (router.query.slug) {
+      const fetchCategoryPosts = async () => {
+        const categoryPosts = await getCategoryPost(router.query.slug);
+        setPosts(categoryPosts);
+      };
+
+      fetchCategoryPosts();
+    }
+  }, [router.query.slug]);
 
   if (router.isFallback) {
     return <Loader />;
@@ -28,23 +40,56 @@ const CategoryPost = ({ posts }) => {
     </div>
   );
 };
+
 export default CategoryPost;
 
-// Fetch data at build time
-export async function getStaticProps({ params }) {
-  const posts = await getCategoryPost(params.slug);
+// import React from "react";
+// import { useRouter } from "next/router";
 
-  return {
-    props: { posts },
-  };
-}
+// import { getCategories, getCategoryPost } from "../../services";
+// import { PostCard, Categories, Loader } from "../../components";
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const categories = await getCategories();
-  return {
-    paths: categories.map(({ slug }) => ({ params: { slug } })),
-    fallback: true,
-  };
-}
+// const CategoryPost = ({ posts }) => {
+//   const router = useRouter();
+
+//   if (router.isFallback) {
+//     return <Loader />;
+//   }
+
+//   return (
+//     <div className="container mx-auto px-10 mb-8">
+//       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+//         <div className="col-span-1 lg:col-span-8">
+//           {posts.map((post, index) => (
+//             <PostCard key={index} post={post.node} />
+//           ))}
+//         </div>
+//         <div className="col-span-1 lg:col-span-4">
+//           <div className="relative lg:sticky top-8">
+//             <Categories />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default CategoryPost;
+
+// // Fetch data at build time
+// export async function getStaticProps({ params }) {
+//   const posts = await getCategoryPost(params.slug);
+
+//   return {
+//     props: { posts },
+//   };
+// }
+
+// // Specify dynamic routes to pre-render pages based on data.
+// // The HTML is generated at build time and will be reused on each request.
+// export async function getStaticPaths() {
+//   const categories = await getCategories();
+//   return {
+//     paths: categories.map(({ slug }) => ({ params: { slug } })),
+//     fallback: true,
+//   };
+// }
